49872b126803501687f2ea0c34c239cefd3a459e
Yazzz28 <yacinemennaa@gmail.com>
2023-11-15 06:21:50 +0100

    quete test automatisé

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,8 +4,22 @@ namespace App;
 
 class Calculator
 {
-    public function sum(int $firstValue, int $secondValue ): int 
+    public function sum(int $firstValue, int $secondValue): int
     {
         return $firstValue + $secondValue;
+    }
+
+    public function multiplication(int $firstValue, int $secondValue): int
+    {
+        return $firstValue * $secondValue;
+    }
+
+    public function isEven(int $value): bool
+    {
+        if ($value % 2 === 0) {
+            return true;
+        } else if ($value % 2 === 1) {
+            return false;
+        }
     }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
-    public function testSum(): void
+   public function testSum(): void
     {
         //Arrange
         $firstValue = 5;
@@ -20,6 +20,39 @@ class CalculatorTest extends TestCase
         //Assert
         $this->assertSame($expectedResult, $sum, 
             "La méthode sum() ne fonctionne plus :-("
+        );
+    }
+
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 2;
+        $expectedResult = 10;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($firstValue, $secondValue);
+        
+        //Assert
+        $this->assertSame($expectedResult, $multiplication, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+
+    public function testIsEven(): void
+    {
+        //Arrange
+        $value = 5;
+        $expectedResult = false;
+
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($value);
+        
+        //Assert
+        $this->assertSame($expectedResult, $isEven, 
+            "La méthode isEven() ne fonctionne plus :-("
         );
     }
 }
